54bd833382aba744b9f59a398a80057a6f7c313c
osoken <osoken.devel@outlook.jp>
2022-02-27 21:04:45 +0900

    wip(app): add static data into nginx static directory

--- app/docker-entrypoint.sh
+++ app/docker-entrypoint.sh
@@ -16,4 +16,16 @@ rm -rf /static/server-backend
 mkdir -p /static/server-backend
 cp -a /server-backend/staticfiles/* /static/server-backend
 
+rm -rf /static/server-frontend
+mkdir -p /static/server-frontend
+cp -a /server-frontend/* /static/server-frontend
+
+rm -rf /static/server-site
+mkdir -p /static/server-site
+cp -a /server-site/* /static/server-site
+
+rm -rf /static/client-frontend
+mkdir -p /static/client-frontend
+cp -a /client-frontend/* /static/client-frontend
+
 python manage.py runserver 0.0.0.0:8000
