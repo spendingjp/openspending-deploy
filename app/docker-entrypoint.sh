6dc845327985787a933b57c1ad65551fbbf4db9e
osoken <osoken.devel@outlook.jp>
2022-02-27 18:54:41 +0900

    wip(app): change structure

---
+++ app/docker-entrypoint.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+set -e
+
+cd /openspending-backend
+git pull origin develop
+
+cd /server-backend
+python manage.py makemigrations
+python manage.py makemigrations budgetmapper
+python manage.py migrate
+python manage.py loaddata budgetmapper/fixtures/budget_xlsx_template.json
+python manage.py loaddata budgetmapper/fixtures/iconimages.json
+python manage.py collectstatic --no-input
+
+rm -rf /static/server-backend
+mkdir -p /static/server-backend
+cp -a /server-backend/staticfiles/* /static/server-backend
+
+python manage.py runserver 0.0.0.0:8000
